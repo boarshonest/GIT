9df621f5cd2b12144dcda5ccfc2b15605dcbc203
Jyo <hkboars@gmail.com>
2025-02-24 17:05:05 +0100

    Cambio nueva linea en nueva rama

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,7 @@
 public class HolaMundo {
-    
+
+    public static void main (String[] args) {
+    System.out.println("Hola Mundo");  // Comentario hater para probar git
+    System.out.println("Estamos en otra rama");
+    }
 }
